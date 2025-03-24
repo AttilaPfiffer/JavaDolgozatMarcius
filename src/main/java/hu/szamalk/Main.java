a86454e1ca65a995c9a74066035058e23f2a1bbf
AttilaPfiffer <pifuati@gmial.com>
2025-03-24 14:12:35 +0100

    rendezesek

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -1,7 +1,25 @@
 package hu.szamalk;
 
+import hu.szamalk.modell.Szak;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static List<Szak> szakokLista = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Szak.szakKiirasa();
+
+        System.out.println("Rendezesek: ");
+        szakokLista.sort(Szak.getTargyakNevSzerint());
+        System.out.println("---- \nnév szerint: ");
+
+
+        szakokLista.sort(Szak.getTargyakKreditSzerint());
+        System.out.println("---- \nkredit szerint: ");
     }
 }
